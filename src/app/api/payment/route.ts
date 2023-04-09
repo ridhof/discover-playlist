b2a0319f5722ff04ee0e3921c683e91a163f2efa
Ridho Febriansa <Ridho Febriansa>
2023-04-09 07:54:09 +0700

    feat: add midtrans request payment endpoint

--- src/app/api/payment/route.ts
+++ src/app/api/payment/route.ts
@@ -1,4 +1,50 @@
-export async function GET(request: Request) {
+import { NextResponse } from "next/server";
 
-  return new Response('Hello, Payment Gateway!');
+type RequestData = {
+  order_id: string;
+  gross_amount: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+};
+
+type Transaction = {
+  redirect_url: string;
+  token: string;
+};
+
+export async function POST(request: Request) {
+  try {
+    const reqData: RequestData = await request.json();
+    const midtransClient = require("midtrans-client");
+    const snap = new midtransClient.Snap({
+      isProduction: process.env.IS_PRODUCTION === 'true',
+      serverKey: process.env.MIDTRANS_SERVER_KEY,
+    });
+    const transaction: Transaction = await snap.createTransaction({
+      transaction_details: {
+        order_id: reqData.order_id,
+        gross_amount: reqData.gross_amount
+      },
+      credit_card: { secure: true },
+      customer_details: {
+        first_name: reqData.first_name,
+        last_name: reqData.last_name,
+        email: reqData.email
+      },
+    });
+    return NextResponse.json({
+      ...transaction,
+      error: false,
+      error_message: null,
+    });
+  } catch (err) {
+    console.debug('err');
+    console.debug(err);
+    return NextResponse.json({
+      error: true,
+      error_message: "Unable to connect to Payment Service",
+      data: null,
+    });
+  }
 }
